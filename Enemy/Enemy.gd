48e01b8debf43b3226e02eb2a386a02449bf966f
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2024-01-09 10:27:25 +0700

    feat: balance enemy

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -35,6 +35,8 @@ func load_stat():
 	speed = res.speed
 	damage = res.damage
 	$move.wait_time = 1.0/speed
+	$shoot.wait_time = 20.0/damage
+	print($shoot.wait_time)
 
 func _ready():
 	rng.randomize()
